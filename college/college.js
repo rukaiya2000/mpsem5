f719a53dc3d03bd348d938a77535be6b68f4e955
Rukaiya Khan <rukaiyak2000@gmail.com>
2021-11-29 10:00:14 +0530

    fixed updates and college section

--- college/college.js
+++ college/college.js
@@ -32,3 +32,35 @@ function colorLink(){
 } 
 linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
+// document.getElementById('id').click(function(){
+//     console.log("aaa");
+// var value = $("#id").val();
+// window.location.href = 'www.youtube.com';
+//     return false;
+    
+// });
+
+document.getElementById('submit1').onclick = function() {
+    var value = document.getElementById("id1").value;
+    console.log(value);
+    var redirectUrl = "https://stackoverflow.com/nocaptcha?s=" + value;
+    window. open(redirectUrl, '_blank');     
+    return false;
+ 
+ };
+ document.getElementById('submit2').onclick = function() {
+    var value = document.getElementById("id2").value;
+    console.log(value);
+    var redirectUrl = "http://youtube.com/results?search_query=" + value;
+    window. open(redirectUrl, '_blank');     
+    return false;
+ 
+ };
+ document.getElementById('submit3').onclick = function() {
+    var value = document.getElementById("id3").value;
+    console.log(value);
+    var redirectUrl = "https://www.google.com/search?q=" + value;
+    window. open(redirectUrl, '_blank');     
+    return false;
+ 
+ };
